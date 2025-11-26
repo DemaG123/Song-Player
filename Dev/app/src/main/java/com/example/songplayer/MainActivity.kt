18217de2d04a5d2b3db83dd35119f77c1abd7763
Ahmed Gacem <gacema426@gmail.com>
2025-11-26 14:21:55 -0500

    Add : separating the populating of the ListView in the Modele.kt and MainActivity.kt

--- Dev/app/src/main/java/com/example/songplayer/MainActivity.kt
+++ Dev/app/src/main/java/com/example/songplayer/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity(), Observateur {
 
     lateinit var liste : ListView
     val url = "https://api.jsonbin.io/v3/b/69090376ae596e708f425664?meta=false"
-    //var monModele = Modele(this)
+    var monModele = Modele(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,59 +37,10 @@ class MainActivity : AppCompatActivity(), Observateur {
 
         liste = findViewById(R.id.liste)
 
+        monModele.chercherChansons(liste)
+
         //monModele.ajouterObservateur(this)
 
-        val queue = Volley.newRequestQueue(this)
-
-        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
-            { response ->
-                val tab = response.getJSONArray("music")
-                decomposerReponse(tab)
-            },
-            { error ->
-                Toast.makeText(this, "That didn't work!", Toast.LENGTH_SHORT).show()
-                error.printStackTrace()
-            }
-        )
-
-        queue.add(jsonObjectRequest)
-    }
-
-    fun decomposerReponse(tab : JSONArray) {
-        val remplir = ArrayList<HashMap<String, String>>()
-
-        for (i in 0..tab.length()-1) {
-            var temp = HashMap<String, String>()
-            //temp.put("id", tab.getJSONObject(i).get("id").toString())
-            temp.put("title", tab.getJSONObject(i).get("title").toString())
-            temp.put("album", tab.getJSONObject(i).get("album").toString())
-            temp.put("artist", tab.getJSONObject(i).get("artist").toString())
-            //temp.put("genre", tab.getJSONObject(i).get("genre").toString())
-            //temp.put("source", tab.getJSONObject(i).get("source").toString())
-            temp.put("image", tab.getJSONObject(i).get("image").toString())
-            //temp.put("trackNumber", tab.getJSONObject(i).get("trackNumber").toString())
-            //temp.put("totalTrackCount", tab.getJSONObject(i).get("totalTrackCount").toString())
-            //temp.put("duration", tab.getJSONObject(i).get("duration").toString())
-            //temp.put("site", tab.getJSONObject(i).get("site").toString())
-
-            remplir.add(temp)
-        }
-        // créer un SimpleAdapter
-        val adapter = MonAdapter(this, remplir, R.layout.item, arrayOf("title","album","artist","image"), intArrayOf(R.id.textTitle, R.id.textAlbum, R.id.textArtist,R.id.image))
-
-        //val adapter = SimpleAdapter(this, remplir, R.layout.test, arrayOf("title", "album","artist"), intArrayOf(R.id.test, R.id.test2, R.id.test3))
-        //val maliste = listOf("sdfg","sdfg","sdfg")
-        //val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1 , maliste)
-
-        liste.adapter = adapter
-
-    }
-
-    inner class MonAdapter (context: Context, data: List<Map<String, Any>>, resource: Int, from: Array<String>, to: IntArray) :SimpleAdapter( context,data, resource, from, to ){
-        override fun setViewImage (v: ImageView, value:String) {
-            Glide.with(this@MainActivity).load(value).into(v);
-
-        }
     }
 
     override fun onDestroy() {
--- Dev/app/src/main/java/com/example/songplayer/Modele.kt
+++ Dev/app/src/main/java/com/example/songplayer/Modele.kt
@@ -1,45 +1,78 @@
 package com.example.songplayer
 
 import android.content.Context
+import android.widget.ImageView
+import android.widget.ListView
+import android.widget.SimpleAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.beust.klaxon.Klaxon
+import com.bumptech.glide.Glide
+import org.json.JSONArray
 
 class Modele(var context : Context) : Sujet{
 
     private lateinit var lesChansons: List<ListeChansons>
     private var obs: Observateur? = null
 
-    fun chercherChansons() {
+    fun chercherChansons(liste: ListView) {
         val queue = Volley.newRequestQueue(context)
         val url = "https://api.jsonbin.io/v3/b/69090376ae596e708f425664?meta=false"
 
-        // Notre requête demande une String du serveur
-        val stringRequest = StringRequest(
-            Request.Method.GET, url,
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
             { response ->
-                val listeProduits:ListeChansons = Klaxon().parse<ListeChansons>(response) ?: ListeChansons()
-                Toast.makeText(context, listeProduits.chansons.size.toString(), Toast.LENGTH_SHORT).show()
-                lesChansons = listeProduits.chansons
-
+                val tab = response.getJSONArray("music")
+                decomposerReponse(tab, liste)
             },
-            { Toast.makeText(context, "That didn't work!", Toast.LENGTH_SHORT).show() })
+            { error ->
+                Toast.makeText(context, "That didn't work!", Toast.LENGTH_SHORT).show()
+                error.printStackTrace()
+            }
+        )
+        queue.add(jsonObjectRequest)
+    }
 
-        // Ajouter notre requête à la queue.
-        queue.add(stringRequest)
+    fun decomposerReponse(tab : JSONArray, liste : ListView) {
+        val remplir = ArrayList<HashMap<String, String>>()
+
+        for (i in 0..tab.length()-1) {
+            var temp = HashMap<String, String>()
+            //temp.put("id", tab.getJSONObject(i).get("id").toString())
+            temp.put("title", tab.getJSONObject(i).get("title").toString())
+            temp.put("album", tab.getJSONObject(i).get("album").toString())
+            temp.put("artist", tab.getJSONObject(i).get("artist").toString())
+            //temp.put("genre", tab.getJSONObject(i).get("genre").toString())
+            //temp.put("source", tab.getJSONObject(i).get("source").toString())
+            temp.put("image", tab.getJSONObject(i).get("image").toString())
+            //temp.put("trackNumber", tab.getJSONObject(i).get("trackNumber").toString())
+            //temp.put("totalTrackCount", tab.getJSONObject(i).get("totalTrackCount").toString())
+            //temp.put("duration", tab.getJSONObject(i).get("duration").toString())
+            //temp.put("site", tab.getJSONObject(i).get("site").toString())
+
+            remplir.add(temp)
+        }
+        val adapter = MonAdapter(context, remplir, R.layout.item, arrayOf("title","album","artist","image"), intArrayOf(R.id.textTitle, R.id.textAlbum, R.id.textArtist,R.id.image))
+        liste.adapter = adapter
+
+    }
+
+    inner class MonAdapter (context: Context, data: List<Map<String, Any>>, resource: Int, from: Array<String>, to: IntArray) :
+        SimpleAdapter( context,data, resource, from, to ){
+        override fun setViewImage (v: ImageView, value:String) {
+            Glide.with(context).load(value).into(v);
+        }
     }
 
     fun getUneChanson(): List<ListeChansons>? {
         return lesChansons
     }
 
-
     fun setValeur(lesChansons: List<ListeChansons>) {
         this.lesChansons = lesChansons
-        //changement de l'état du sujet, avertir les observateurs
         avertirObservateurs()
     }
 
@@ -54,6 +87,5 @@ class Modele(var context : Context) : Sujet{
     override fun avertirObservateurs() {
         obs!!.changement(lesChansons)
     }
-
 
 }
